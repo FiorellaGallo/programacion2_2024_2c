c814727b0b2e117dc6d69fda1c812b436b4ef666
María Agustina Ortiz <95661814+agustina-ortiz@users.noreply.github.com>
2024-09-05 16:43:36 -0300

    validaciones

--- Ejercicio10/main.c
+++ Ejercicio10/main.c
@@ -3,6 +3,22 @@
 #include<math.h>
 #include "ejercicio10.h"
 
+void validaciones(int *N, int *B) {
+    int c;
+
+    printf("Ingrese un numero explosivo:\n");
+    while(scanf("%d", N) != 1 || *N <= 0){
+        printf("Por favor ingrese un numero valido: ");
+        while ((c = getchar()) != '\n' && c != EOF);
+    };
+
+    printf("Ingrese un numero bomba(menor al numero explosivo):\n");
+    while(scanf("%d", B) != 1 || *B <= 0 || *B >= *N){
+        printf("Por favor ingrese un numero valido: ");
+        while ((c = getchar()) != '\n' && c != EOF);
+    };
+}
+
 
 int main()
 {
@@ -10,12 +26,7 @@ int main()
     int B = 0;
     int size = 0;
 
-    printf("Ingrese un número explosivo:\n");
-    scanf("%d",&N);
-    while(fgetc(stdin) != '\n');
-    printf("Ingrese un número bomba(menor al número de explosivos):\n");
-    scanf("%d",&B);
-    while(fgetc(stdin) != '\n');
+    validaciones(&N, &B);
 
     int* fragmentos = explosion(N,B,&size);
 
