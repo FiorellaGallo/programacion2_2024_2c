b401591547022710e0da96fc01a484fe2f5151f2
FiorellaGallo <fioreg96@gmail.com>
2024-09-05 21:46:02 -0300

    Trabajo finalizado

--- ejercicio-6/main.c
+++ ejercicio-6/main.c
@@ -5,31 +5,30 @@
 int main()
 {
     int chinos;
+    printf("\nIngrese el nivel de la delegacion china que desea ver entre 1 y 50. Ingrese 0 para salir del sistema: ");
 
-    printf("\nIngrese el nivel de la delegación china que desea ver. Ingrese 0 para salir del sistema: ");
-    scanf("%d", &chinos);
-    fflush(stdin);
-
-    if (chinos == 0) {printf("Saliste."); return 0;}
-
-    while (chinos != 0){
-        if(chinos > 50 || chinos < 1){
-            printf("\nNivel de delegación fuera de rango, ingrese nuevamente (0 para salir del sistema): ");
-            scanf("%d", &chinos);
+    while (1) {
+        if (scanf("%d", &chinos) != 1) {
             fflush(stdin);
-        } else {
-            printf("La delegación china de nivel %d se ve así: \n", chinos);
-            delegacion(chinos);
-            printf("\n\nIngrese el nivel de la delegación china que desea ver. Ingrese 0 para salir del sistema: ");
-            scanf("%d", &chinos);
-            fflush(stdin);
+            while(fgetc(stdin) != '\n');
+
+            printf("\Dato incorrecto. Ingrese un numero entre 1 y 50 (0 para salir): ");
+            continue;
         }
-        if (chinos == 0){
-            printf("Saliste del sistema.");
+
+        if (chinos == 0) {
+            printf("\nSaliste del sistema.\n");
             return 0;
         }
-    }
+        if (chinos < 1 || chinos > 50) {
+            printf("Nivel de delegacion fuera de rango. Ingrese un numero entre 1 y 50 (0 para salir): ");
+            continue;
+        }
+        printf("La delegacion china de nivel %d se ve asi:\n", chinos);
+        delegacion(chinos);
 
+        printf("\nIngrese el nivel de la delegacion china que desea ver. Ingrese 0 para salir del sistema: ");
+    }
 
     return 0;
 }
